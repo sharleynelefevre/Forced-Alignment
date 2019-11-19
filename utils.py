0a0fc4946c98d42a5973f313be322e0d356526e8
paullerner <1.paul.lerner@gmail.com>
2019-11-19 16:33:33 +0100

    feat: manage user input with default answer

--- utils.py
+++ utils.py
@@ -5,3 +5,27 @@ def normalize_string(string):
     string =re.sub(r"([,.!?'-:])", r"", string).lower()
     string = re.sub(' +', ' ',string).strip()
     return string
+
+def do_this(display="",defaults_to_yes=True):
+    """
+    Parameters:
+    -----------
+    display: `str` to display to the user. Defaults to ""
+    defaults_to_yes: `bool`: default answer if the user doesn't input anything
+
+    Returns:
+    --------
+    True if the user wants to do it, else False.
+    """
+    answer=input("{} {}".format(display,"[Y]/N" if defaults_to_yes else "Y/[N]"))
+    answer=answer.lower().strip()
+    if defaults_to_yes:
+        if answer != 'n' and answer != 'no':
+            return True
+        else:
+            return False
+    else:
+        if answer != 'y' and answer != 'yes':
+            return True
+        else:
+            return False
