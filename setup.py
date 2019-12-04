c423eadb8caf17e34999a6e70506c0f07ad03646
paullerner <1.paul.lerner@gmail.com>
2019-12-04 17:21:47 +0100

    ci: add setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(name='forced_alignment',
+      version='1.0',
+      packages=find_packages(),
+     # packages=['convert','forced-alignment'],
+      author='Paul Lerner',
+      author_email='1.paul.lerner@gmail.com',
+      )
